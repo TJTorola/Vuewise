4634cbd70d8f6c6dda5debcdeb737fd226d6cfa0
Tyler Torola <tyler@tjt.codes>
2017-02-09 22:17:41 -0800

    Fix requires

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const icon        = require('~components/icon/basic'),
-      workingIcon = require('~components/icon/working');
+const icon        = require('./components/icon/basic'),
+      workingIcon = require('./components/icon/working');
 
-const box    = require('~components/box/basic'),
-      button = require('~components/button/basic'),
-      input  = require('~components/input/basic');
+const box    = require('./components/box/basic'),
+      button = require('./components/button/basic'),
+      input  = require('./components/input/basic');
 
 module.exports = {
   icon,
