0b3c4cc6b4783b3fdd5d8dfa5d6ac2143c354ea2
Tyler Torola <tyler@tjt.codes>
2017-02-09 17:11:21 -0800

    Adds basic icon component and Font Awesome SVG assets

---
+++ components/icon/basic.vue
@@ -0,0 +1,28 @@
+<template>
+	<svg
+		viewBox="0 0 32 32"
+		xmlns="http://www.w3.org/2000/svg"
+		class="icon">
+		<path v-bind:d="pathData" />
+	</svg>
+</template>
+
+<script>
+import FA from '~assets/js/fa.js';
+
+export default {
+	props: ['i'],
+
+	computed: {
+		pathData() {
+			return FA[this.i];
+		}
+	}
+}
+</script>
+
+<style scoped>
+svg {
+	height: 32px;
+}
+</style>
